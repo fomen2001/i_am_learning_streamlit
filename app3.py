be71a8f8d0115bb863cf5b26213127e3cd1ad009
fomen2001 <118531985+fomen2001@users.noreply.github.com>
2025-04-28 23:32:55 +0200

    module1

---
+++ app3.py
@@ -0,0 +1,22 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+
+data = np.random.normal(size=1000)
+data = pd.DataFrame(data, columns=["Dist_norm"])
+#st.write(data.head()) 
+st.dataframe(data.head())
+
+fig, ax = plt.subplots()
+n_bins = st.number_input(
+    label="choisis un nombre de bins",
+    min_value=10,
+    value=20
+)
+ax.hist(data.Dist_norm, bins=n_bins)
+title = st.text_input(
+    label ="ecrire le titre du graphique"
+)
+plt.title(title)
+st.pyplot(fig)
